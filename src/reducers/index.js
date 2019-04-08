1e6d432353d2abfbad173fc4456104b6659b5035
erica-y-chen <eyufanchen@gmail.com>
2019-04-08 13:59:44 -0700

    Completed counter

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -12,10 +12,20 @@ const initialState = {
 export default (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
-    // Fill in the body of this case
+    console.log("Test")
+      return {
+        ...state, 
+        count: state.count + 1,
+      }
+
     case DECREMENT:
-    // Fill in the body of this case
+    return {
+      ...state, 
+      count: state.count - 1,
+    }
+
     default:
+    console.log(state);
       return state;
   }
 };
